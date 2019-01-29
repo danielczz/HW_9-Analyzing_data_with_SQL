9782ec373ff7e44a0c907f4029f6914b4b2e324b
Daniel Cespedes Zorob <daniel.cespedes.zorob@gmail.com>
2019-01-28 21:13:34 -0600

    Commitment to advance 3 points.

--- main_script_Analyzing_data_with_SQL.sql
+++ main_script_Analyzing_data_with_SQL.sql
@@ -75,23 +75,98 @@ FROM actor
 GROUP BY last_name
 HAVING actors_with_last_name_greater_than_2 >= 2;
 
+# 4c. The actor HARPO WILLIAMS was accidentally entered in the actor table as 
+# GROUCHO WILLIAMS. Write a query to fix the record.
+UPDATE actor
+SET first_name='HARPO'
+WHERE first_name = 'GROUCHO' AND last_name = 'WILLIAMS';
+
+SELECT first_name, last_name FROM actor 
+WHERE first_name = 'HARPO' AND last_name = 'WILLIAMS';
+
+# 4d. Perhaps we were too hasty in changing GROUCHO to HARPO. It turns out that 
+# GROUCHO was the correct name after all! In a single query, if the first name of the actor
+# is currently HARPO, change it to GROUCHO.
+UPDATE actor
+SET first_name='GROUCHO'
+WHERE first_name = 'HARPO' AND last_name = 'WILLIAMS';
+
+SELECT first_name, last_name FROM actor 
+WHERE first_name = 'GROUCHO' AND last_name = 'WILLIAMS';
+
+# 5a. You cannot locate the schema of the address table. Which query would you use to re-create it?
+SELECT *
+FROM INFORMATION_SCHEMA.COLUMNS 
+WHERE TABLE_SCHEMA = 'sakila' AND TABLE_NAME ='address'; 
+
+# 6a. Use JOIN to display the first and last names, as well as the address, of each staff member. 
+# Use the tables staff and address:
+SELECT 
+staff.first_name, staff.last_name, 
+address.address FROM staff INNER JOIN address ON staff.address_id = address.address_id;
+
+# 6b. Use JOIN to display the total amount rung up by each staff member in August of 2005. 
+# Use tables staff and payment.
+SELECT first_name, last_name, SUM(amount) AS 'Amount by each staff member'
+FROM staff
+INNER JOIN payment
+ON staff.staff_id = payment.staff_id
+WHERE payment.payment_date LIKE ( '2005-08-%')
+GROUP BY payment.staff_id;
+
+# 6c. List each film and the number of actors who are listed for that film. 
+# Use tables film_actor and film. Use inner join.
+SELECT title, COUNT(actor_id) AS 'Amount of actors by film'
+FROM film
+INNER JOIN film_actor
+ON film.film_id = film_actor.film_id
+GROUP BY film_actor.film_id;
+
+# 6d. How many copies of the film Hunchback Impossible exist in the inventory system?
+SELECT title, COUNT(inventory_id) AS 'Amount of films by inventory ID'
+FROM film
+INNER JOIN inventory
+ON film.film_id = inventory.film_id
+WHERE title = 'Hunchback Impossible' 
+GROUP BY inventory.film_id;
+
+# 6e. Using the tables payment and customer and the JOIN command, list the total paid by each customer. List the customers alphabetically by last name:
+SELECT first_name, last_name, SUM(amount) AS 'Total amount paid by each customer'
+FROM customer
+INNER JOIN payment
+ON customer.customer_id = payment.customer_id
+GROUP BY payment.customer_id
+ORDER BY last_name;
+
+
+# 
+# 
+
+
+
+/*
+
+
+
+
+
+
+7a. The music of Queen and Kris Kristofferson have seen an unlikely resurgence. As an unintended consequence, films starting with the letters K and Q have also soared in popularity. Use subqueries to display the titles of movies starting with the letters K and Q whose language is English.
+7b. Use subqueries to display all actors who appear in the film Alone Trip.
+7c. You want to run an email marketing campaign in Canada, for which you will need the names and email addresses of all Canadian customers. Use joins to retrieve this information.
+7d. Sales have been lagging among young families, and you wish to target all family movies for a promotion. Identify all movies categorized as family films.
+7e. Display the most frequently rented movies in descending order.
+7f. Write a query to display how much business, in dollars, each store brought in.
+7g. Write a query to display for each store its store ID, city, and country.
+7h. List the top five genres in gross revenue in descending order. (Hint: you may need to use the following tables: category, film_category, inventory, payment, and rental.)
+8a. In your new role as an executive, you would like to have an easy way of viewing the Top five genres by gross revenue. Use the solution from the problem above to create a view. If you haven't solved 7h, you can substitute another query to create a view.
+8b. How would you display the view that you created in 8a?
+8c. You find that you no longer need the view top_five_genres. Write a query to delete it.
+
+
 
 
 
 
 
 /*
-4d. Perhaps we were too hasty in changing GROUCHO to HARPO. It turns out that GROUCHO was the correct name after all! In a single query, if the first name of the actor is currently HARPO, change it to GROUCHO.
-
-5a. You cannot locate the schema of the address table. Which query would you use to re-create it?
-
-
-Hint: https://dev.mysql.com/doc/refman/5.7/en/show-create-table.html
-
-
-
-6a. Use JOIN to display the first and last names, as well as the address, of each staff member. Use the tables staff and address:
-6b. Use JOIN to display the total amount rung up by each staff member in August of 2005. Use tables staff and payment.
-6c. List each film and the number of actors who are listed for that film. Use tables film_actor and film. Use inner join.
-6d. How many copies of the film Hunchback Impossible exist in the inventory system?
-6e. Using the tables payment and customer and the JOIN command, list the total paid by each customer. List the customers alphabetically by last name:
